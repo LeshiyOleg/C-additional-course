354574b551d63de569f466b8ab0077373b98685e
Oleg S <leshiyoleg@mail.ru>
2022-05-09 14:14:26 +0300

    Changed Program_1 to calculator

--- Program_1/HelloWorld/Program.cs
+++ Program_1/HelloWorld/Program.cs
@@ -6,8 +6,27 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("I'd like to study C#!");
+            //Console.WriteLine("I'd like to study C#!");
+            //Console.ReadLine();
+
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+
+            int num1 = Int32.Parse(a);
+            int num2 = Int32.Parse(b);
+
+            int result = num1 + num2;
+            Console.WriteLine(result);
+
+            result = num1 * num2;
+            Console.WriteLine(result);
+
+            double average = (double) (num1 + num2) / 2;
+
+            Console.WriteLine(average);
+
             Console.ReadLine();
+
         }
     }
 }

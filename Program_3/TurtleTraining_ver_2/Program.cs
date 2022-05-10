0974a85ba3fb8fcb85d53a864e8ef10141de232f
Oleg S <leshiyoleg@mail.ru>
2022-05-10 14:48:29 +0300

    Added homework in branch Lesson_4_HW_Turtle

--- Program_3/TurtleTraining_ver_2/Program.cs
+++ Program_3/TurtleTraining_ver_2/Program.cs
@@ -12,13 +12,33 @@ namespace TurtleTraining_ver_2
         static void Main(string[] args)
         {
             Turtle.Speed = 9;
-            int i = 0;
-            while (i < 4)
+            /* int n = 0;
+            while (n < 4)
             {
-                Turtle.Move(100);
-                // Turtle.TurnRight();
-                // Turtle.Turn(90);
-                Turtle.TurnLeft();
+                int r = 0;
+
+                while (r < 2)
+                {
+                    Turtle.Move(30);
+                    Turtle.TurnRight();
+                    r++;
+                }
+                int l = 0;
+                while (l < 2)
+                {
+                    Turtle.Move(30);
+                    Turtle.TurnLeft();
+                    l++;
+                }
+                n++; 
+            }
+            */
+            int i = 0;
+
+            while (i<6)
+            {
+                Turtle.Move(50);
+                Turtle.Turn(60);
                 i++;
             }
         }
